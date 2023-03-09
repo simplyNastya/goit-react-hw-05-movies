6e45f4f70658942ae16d97e180435daeb74def57
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-03-10 00:42:22 +0100

    Refactored code
    Added css to navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Cast from './Cast/Cast';
 import Navbar from './Navbar/Navbar';
+import Home from 'pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
 import TrendingMoviesList from './TrendingMoviesList/TrendingMoviesList';
 import Reviews from './Reviews/Reviews';
-import Home from 'pages/Home/Home';
 import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Movies from 'pages/Movies/Movies';
 import SearchMoviesForm from './SearchMoviesForm/SearchMoviesForm';
 import NotFound from 'pages/NotFound/NotFound';
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 // import PropTypes from 'prop-types';
+
 import styles from './navbar.module.css';
 
 const Navbar = () => {
-  return <p className={styles.text}>Navbar</p>;
+  return (
+    <header className={styles.headerSection}>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a
+              href="#"
+              className={`${styles.navLink} ${styles.navLinkCurrent}`}
+            >
+              Home
+            </a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>
+              Movies
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
