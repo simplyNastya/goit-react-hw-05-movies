db10bb30ba7ca584b3087256c394b08d51d7f65f
simplyNastya <116064159+simplyNastya@users.noreply.github.com>
2023-07-14 15:03:06 +0000

    Deploying to gh-pages from @ simplyNastya/goit-react-hw-05-movies@6aafa2e514498fe66b18765de33edecd4c3a9ccc 🚀

---
+++ static/js/29.6b88b853.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[29],{3029:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r="notFoundPage_text__hrPIS",u=n(184),a=function(){return(0,u.jsx)("p",{className:r,children:"NotFound"})}}}]);
+//# sourceMappingURL=29.6b88b853.chunk.js.map
